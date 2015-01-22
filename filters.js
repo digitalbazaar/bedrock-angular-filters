2028020a0beec4d99fc878117e5ec1dd280abd43
Dave Longley <dlongley@digitalbazaar.com>
2015-01-22 14:02:06 -0500

    Add filters module.

---
+++ filters.js
@@ -0,0 +1,47 @@
+/*!
+ * Filters module.
+ *
+ * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
+ *
+ * @author Dave Longley
+ */
+define([
+  'angular',
+  './ceil-filter',
+  './bytes-filter',
+  './ellipsis-filter',
+  './embedded-string-filter',
+  './encodeuricomponent-filter',
+  './floor-filter',
+  './mask-filter',
+  './now-filter',
+  './prefill-filter'
+], function(
+  angular,
+  ceil,
+  bytes,
+  ellipsis,
+  embeddedString,
+  encodeURIComponent_,
+  floor,
+  mask,
+  now,
+  prefill) {
+
+'use strict';
+
+var module = angular.module('app.filters', []);
+
+module.filter(ceil);
+module.filter(bytes);
+module.filter(ellipsis);
+module.filter(embeddedString);
+module.filter(encodeURIComponent_);
+module.filter(floor);
+module.filter(mask);
+module.filter(now);
+module.filter(prefill);
+
+return module.name;
+
+});
